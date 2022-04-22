4731ccd9a89b9d13a8d923da112433ce418eb9f5
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-22 19:02:10 +0900

    feat: add `exit_code` function to exit qemu

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
-    println!("All tests passed!");
+
+    exit_code(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -36,4 +37,20 @@ fn trivial_assertion() {
     print!("trivial assertion... "); // "些末なアサーション……"
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_code(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32); // Use u32 because 4 bytes were specified for iosize
+    }
 }
