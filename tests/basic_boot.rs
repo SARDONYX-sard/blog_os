c7bb4ff712ba229724e1397e7fdd24c73a1d672d
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-23 01:33:07 +0900

    test: add `basic_boot` integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,26 @@
+// The crates of lib.rs, main.rs and integration tests are separated,
+// so we have to write the necessary proc_macro and entry points each time.
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use blog_os::println;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
