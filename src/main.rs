40cc7aaba5b6ac57491cc6b099d8231ea4786823
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-22 18:13:49 +0900

    feat: change buffer to static mut

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again!").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
 
     loop {}
 }
