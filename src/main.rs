ad6d47fd87d4928192830a9ca75bcfdfa4425419
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-25 14:19:59 +0900

    feat(main): add L3 table browsing code

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
