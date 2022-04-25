9684227731a3a06b2167798c86a9ce293c846f65
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-25 14:53:49 +0900

    feat(main): show virtual & physical address

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,33 @@
 use blog_os::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use x86_64::structures::paging::PageTable;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory::activate_level_4_table;
+    use blog_os::memory::translate_addr;
     use x86_64::VirtAddr;
 
     println!("Hello world{}", "!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = activate_level_4_table(phys_mem_offset);
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    let addresses = [
+        // Identity supported VGA buffer page
+        0xb8000,
+        // Somewhere in the codePage
+        0x201008,
+        // Somewhere in the stack page
+        0x0100_0020_1a10,
+        // Virtual address mapped to a physical address "0"
+        boot_info.physical_memory_offset,
+    ];
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = translate_addr(virt, phys_mem_offset);
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
