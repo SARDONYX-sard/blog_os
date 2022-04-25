7784a23aff0033c8516efa55d08c0241665c7b7b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-25 15:15:35 +0900

    feat(main): confirm acquisition of huge page addr

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ use core::panic::PanicInfo;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use blog_os::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
 
     println!("Hello world{}", "!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = memory::init(phys_mem_offset);
 
     let addresses = [
         // Identity supported VGA buffer page
@@ -32,7 +33,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = translate_addr(virt, phys_mem_offset);
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
