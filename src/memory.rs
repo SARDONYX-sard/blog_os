28243c4dc2e8466b2ca874f07048eb0f32623491
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-25 15:13:00 +0900

    feat(memory): impl to create OPT's  wrapper
    Leave the implementation of the huge page calc code to an external crate
    
    OPT == `OffsetPageTable`

--- src/memory.rs
+++ src/memory.rs
@@ -1,16 +1,7 @@
-use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
-
-pub fn activate_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
-    use x86_64::registers::control::Cr3;
-
-    let (level_4_table_frame, _) = Cr3::read();
-
-    let phys = level_4_table_frame.start_address();
-    let virt = physical_memory_offset + phys.as_u64();
-    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
-
-    unsafe { &mut *page_table_ptr }
-}
+use x86_64::{
+    structures::paging::{OffsetPageTable, PageTable},
+    PhysAddr, VirtAddr,
+};
 
 pub fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
     translate_addr_inner(addr, physical_memory_offset)
@@ -46,4 +37,24 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
     }
 
     Some(frame.start_address() + u64::from(addr.page_offset()))
+}
+
+/// Create a new OffsetPageTable.
+///
+/// physical_memory_offset - Virtual address where physical memory mapping begins.
+pub fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    unsafe { OffsetPageTable::new(level_4_table, physical_memory_offset) }
+}
+
+fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let phys = level_4_table_frame.start_address();
+    let virt = physical_memory_offset + phys.as_u64();
+    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    unsafe { &mut *page_table_ptr }
 }
