1fc5edc3ec0c7a6840b7f17128f4e0d1368d0905
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-28 17:10:40 +0900

    Change `ALLOCATOR` to BumpAllocator`

--- src/allocator.rs
+++ src/allocator.rs
@@ -3,11 +3,12 @@ pub mod bump;
 use core::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 
-use linked_list_allocator::LockedHeap;
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::page::PageRange;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::VirtAddr;
+
+use self::bump::BumpAllocator;
 
 pub struct Dummy;
 
@@ -22,7 +23,7 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
