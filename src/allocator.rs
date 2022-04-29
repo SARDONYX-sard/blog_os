d06862fc581a97cf408c7b3ab7eb05280c0b6073
SARDONYX <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-30 05:09:30 +0900

    Feature/implement fixed size allocator (#9)
    * feat(alloc): add `FixedSizeAllocator`
    
    * feat(fixedAlloc): add `fallback_alloc`
    
    * feat(fixedAlloc): add `list_index`
    
    * style: upper `Empty` variable
    
    * feat(fixedAlloc): implement `GlobalAlloc` trait
    
    * feat(alloc): change to  `FixedSizeAllocator`
    
    From `LinkedListAlloc`

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,12 +1,14 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list::LinkedListAllocator;
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::page::PageRange;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::VirtAddr;
 
+use self::fixed_size_block::FixedSizeBlockAllocator;
+
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 
 pub struct Dummy;
@@ -22,7 +24,7 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
