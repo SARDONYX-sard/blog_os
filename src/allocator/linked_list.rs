ad61083f8fc3709491da600bcfea56643a620004
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-30 00:36:20 +0900

    feat(alloc): add `GlobalAlloc`
    For `LinkedListAllocator`

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,5 +1,6 @@
-use super::align_up;
-use core::mem;
+use super::{align_up, Locked};
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::{mem, ptr};
 
 struct ListNode {
     size: usize,
@@ -22,7 +23,7 @@ impl ListNode {
         self.start_addr() + self.size
     }
 }
-struct LinkedListAllocator {
+pub struct LinkedListAllocator {
     head: ListNode,
 }
 
@@ -109,5 +110,46 @@ impl LinkedListAllocator {
 
         // region suitable for allocation
         Ok(alloc_start)
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // perform layout adjustment
+        let (size, align) = LinkedListAllocator::size_align(layout);
+        let mut allocator = self.lock();
+
+        if let Some((region, alloc_start)) = allocator.find_region(size, align) {
+            let alloc_end = alloc_start.checked_add(size).expect("overflow");
+            let excess_size = region.end_addr() - alloc_end;
+            if excess_size > 0 {
+                allocator.add_free_region(alloc_end, excess_size);
+            }
+            alloc_start as *mut u8
+        } else {
+            ptr::null_mut()
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let (size, _) = LinkedListAllocator::size_align(layout);
+
+        self.lock().add_free_region(ptr as usize, size);
+    }
+}
+
+// in src/allocator/linked_list.rs
+impl LinkedListAllocator {
+    /// Adjust the given layout so that the resulting allocated memory
+    /// region is also capable of storing a `ListNode`.
+    ///
+    /// Returns the adjusted size and alignment as a (size, align) tuple.
+    fn size_align(layout: Layout) -> (usize, usize) {
+        let layout = layout
+            .align_to(mem::align_of::<ListNode>())
+            .expect("adjusting alignment failed")
+            .pad_to_align();
+        let size = layout.size().max(mem::size_of::<ListNode>());
+        (size, layout.align())
     }
 }
