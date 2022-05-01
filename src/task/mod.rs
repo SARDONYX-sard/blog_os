6520e63db103b36df9a38f4713ec313b0e33719a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-01 18:45:42 +0900

    feat(task): add `Task` struct

---
+++ src/task/mod.rs
@@ -0,0 +1,23 @@
+use alloc::boxed::Box;
+use core::task::{Context, Poll};
+use core::{future::Future, pin::Pin};
+
+pub mod simple_executer;
+
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()>>>,
+}
+
+impl Task {
+    pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
+        Task {
+            future: Box::pin(future),
+        }
+    }
+}
+
+impl Task {
+    fn poll(&mut self, context: &mut Context) -> Poll<()> {
+        self.future.as_mut().poll(context)
+    }
+}
