365c859ff8d66a1992164c751976524517f8a566
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-05 12:40:37 +0900

    feat: implement one-character display
    Of hardware interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,8 @@
 use lazy_static::lazy_static;
+use pic8259::ChainedPics;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
-use crate::{gdt, hlt_loop, println};
+use crate::{gdt, hlt_loop, print, println};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -13,6 +14,8 @@ lazy_static! {
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
 
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+
         idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt
@@ -21,6 +24,14 @@ lazy_static! {
 
 pub fn init_idt() {
     IDT.load();
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
@@ -45,6 +56,28 @@ extern "x86-interrupt" fn page_fault_handler(
     println!("Error Code: {:?}", _error_code);
     println!("{:#?}", stack_frame);
     hlt_loop();
+}
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
 }
 
 #[test_case]
