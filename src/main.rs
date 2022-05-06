97a36d8b504315759698e010fac7998b35d82ec8
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-06 21:18:36 +0900

    feat(main): change the `Executor` to use

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ extern crate alloc;
 
 use blog_os::allocator;
 use blog_os::println;
-use blog_os::task::keyboard;
-use blog_os::task::{simple_executer::SimpleTaskExecutor, Task};
+use blog_os::task::executor::Executor;
+use blog_os::task::{keyboard, Task};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
@@ -29,7 +29,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let mut executor = SimpleTaskExecutor::new();
+    let mut executor = Executor::new();
     executor.spawn(Task::new(example_task()));
     executor.spawn(Task::new(keyboard::print_keypresses()));
     executor.run();
