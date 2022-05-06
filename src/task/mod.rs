f87984d513bc56b0b31d567e6cc436c760786825
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-06 16:53:13 +0900

    feat: create `TaskId` struct

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,4 +1,5 @@
 use alloc::boxed::Box;
+use core::sync::atomic::{AtomicU64, Ordering};
 use core::task::{Context, Poll};
 use core::{future::Future, pin::Pin};
 
@@ -6,12 +7,14 @@ pub mod keyboard;
 pub mod simple_executer;
 
 pub struct Task {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
 
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
         Task {
+            id: TaskId::new(),
             future: Box::pin(future),
         }
     }
@@ -20,5 +23,15 @@ impl Task {
 impl Task {
     fn poll(&mut self, context: &mut Context) -> Poll<()> {
         self.future.as_mut().poll(context)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+impl TaskId {
+    pub fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
     }
 }
